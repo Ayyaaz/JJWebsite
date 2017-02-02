8ba9e2141dea44aed3f5eb2121ee9ab7ae9d43cb
Ayyaaz <ayyaaz@doublejames.com>
2017-02-02 14:25:12 +0000

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// npm install -g browser-sync - a global install for browser-sync
+
+// Set-up your package.json
+// $ npm init
+
+// Then Gulp & Packages
+// $ npm install gulp gulp-sass gulp-postcss autoprefixer cssnano gulp-notify gulp-rename del browser-sync --save-dev
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    del = require('del');
+    browserSync = require('browser-sync').create();
+
+// SCSS Compiling Magic
+gulp.task('styles', function () {
+
+    // Auto Prefixer and CSS minification
+    var processors = [
+        autoprefixer('last 2 version'),
+        cssnano
+    ];
+
+    return gulp.src('scss/core.scss') // only reference the central .scss file
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream()) // Exclude if you don't want to use Browser Sync
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('clean', function() {
+    return del(['css']);
+});
+
+gulp.task('watch', function() {
+
+    // Create Browser Sync Server
+    browserSync.init({
+        server: "./"
+    });
+    // Create Browser Sync Server
+
+    // Watch .scss files and reload via Browser Sync
+    gulp.watch('**/*.scss', ['styles']);
+    gulp.watch("./*.html").on('change', browserSync.reload);  // Exclude if you don't want to use Browser Sync (looks at html file changes)
+    
+});
---
+++ js/script.js
@@ -0,0 +1,144 @@
+$(function() {
+
+	// Testimonial Carousel
+	$(function() {
+		$('.slider').slick({
+			dots: true
+			,adaptiveHeight: true
+			,autoplay:true
+			//,autoplaySpeed:4000
+			,prevArrow: ''//<div class="slick-prev"><i class="material-icons">&#xE314;</i></div>'
+			,nextArrow: ''//<div class="slick-next"><i class="material-icons">&#xE315;</i></div>'
+		});
+	});
+	// Testimonial Carousel
+
+	$(".small-menu-trigger").click(function(){
+			$('body').addClass('navOpen');
+			return false;
+	});
+
+	$(".navSmoker").click(function(){
+			$('body').removeClass('navOpen');
+	});
+
+	//smoker
+	$('.smoke').click(function(){
+			if($(this).hasClass('closer')) {
+				$(this).parents('.smoker').removeClass('on');
+			}
+			else
+			{
+				windowID = $(this).attr('href');
+				if(windowID) {
+					$(windowID+'.smoker').toggleClass('on');
+					$(windowID+'.smoker').scrollTop(0); // Makes sure you're at the top of the modal whenever it's launched / relaunched
+					windowID = null;
+				}
+				else
+				{
+					$('.smoker').toggleClass('on');
+					windowID = null;
+				}
+			}
+
+			$('body, html').toggleClass('locked');
+			return false;
+	});
+	//end: smoker
+
+	// Form stuff
+	$('input, textarea').placeholder();
+
+	$('input').focus(function(){
+		$(this).removeClass('error');
+	});
+	// end: Form stuff
+
+
+	/**
+	 * Determine the mobile operating system.
+	 * This function either returns 'iOS', 'Android' or 'unknown'
+	 *
+	 * @returns {String}
+	 */
+	function getMobileOperatingSystem() {
+		var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+		if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) ){
+			return 'platformiOS';
+		}
+		else if( userAgent.match( /Android/i ) ){
+			return 'platformAndroid';
+		}
+		else{
+			return 'platformGeneral';
+		}
+	}
+	var platform = getMobileOperatingSystem();
+	$('body').addClass(platform);
+	// END: mobile OS sniffer
+
+
+
+	// animate on scroll
+	$.fn.visible = function(partial) {
+
+		var $t            = $(this),
+			$w            = $(window),
+			viewTop       = $w.scrollTop(),
+			viewBottom    = viewTop + $w.height(),
+			_top          = $t.offset().top,
+			_bottom       = _top + $t.height(),
+			compareTop    = partial === true ? _bottom : _top,
+			compareBottom = partial === true ? _top : _bottom;
+
+		return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+	};
+
+	var allMods = $(".js-scroll-move");
+
+	allMods.each(function(i, el) {
+		var el = $(el);
+		if (el.visible(true)) {
+			el.addClass("already-visible");
+		}
+	});
+
+	$(window).scroll(function(event) {
+
+		allMods.each(function(i, el) {
+			var el = $(el);
+			if (el.visible(true)) {
+				el.addClass("come-in");
+			}
+		});
+
+	});
+	// end: animate on scroll
+
+
+
+	$('.map-location').hover(
+		function() { // mouse enter
+			$(this).find('.popover').addClass('correct-pos');
+		}, function() { // mouse leave
+			$(this).find('.popover').on('transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd', function() {
+					$(this).removeClass('correct-pos').off('transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd');
+				}
+			);
+		}
+	);
+
+
+
+
+
+
+
+
+
+});
+
+
